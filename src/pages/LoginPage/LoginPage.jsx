26a2df8b890d485ef8788d0fc5f07b4ddbb44a3d
Alina Romanova <alinakhraban@gmail.com>
2023-01-12 15:12:54 +0200

    add edits

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,13 +1,11 @@
 import styles from './LoginPage.module.css';
 import LoginForm from './LoginForm';
-import ButtonAddTransactions from 'components/ButtonAddTransactions/ButtonAddTransactions';
 
 const LoginPage = () => {
   return (
     <div className={styles.registration_page}>
       <div className="container">
         <h1 className={styles.title}>Finance App</h1>
-
         <LoginForm />
       </div>
     </div>
