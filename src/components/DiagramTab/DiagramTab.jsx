2596a9cee9f77b09151de56851272a3a9192d525
Lukast2008 <lukast2008@gmail.com>
2023-01-12 22:21:49 +0200

    fix problems

--- src/components/DiagramTab/DiagramTab.jsx
+++ src/components/DiagramTab/DiagramTab.jsx
@@ -3,86 +3,98 @@ import { Chart as ChartJS, ArcElement, Tooltip } from 'chart.js';
 import { Doughnut } from 'react-chartjs-2';
 import { nanoid } from '@reduxjs/toolkit';
 import styles from './DiagramTab.module.css';
-import { fetchTransactionsSummaryOfPeriod } from './operation';
+import {
+  fetchTransactionsSummaryOfPeriod,
+  fetchTransactionsSummary,
+} from './operation';
 import { useSelector } from 'react-redux';
-
 import { selectStatistic } from './Selector';
 
+import { useDispatch } from 'react-redux';
 
 ChartJS.register(ArcElement, Tooltip);
 
 const DiagramTab = () => {
-
-
-
-
-
   const [month, setMonth] = useState('');
   const [year, SetYear] = useState('2023');
+  const dispatch = useDispatch();
 
-  const dataBASE = useSelector(selectStatistic)
+  const dataBASE = useSelector(selectStatistic);
 
-  
 
-  const DiagramaItem = dataBASE.categoriesSummary.filter(({ type }) =>
-    type.includes('EXPENSE')
-  );
+  let DiagramaItem = null;
+  let ExpenseSum = null;
+  let TitleExpense = null;
+  let visible = false;
+  let data = null;
 
-  const ExpenseSum = dataBASE.categoriesSummary
-    .filter(({ type }) => type.includes('EXPENSE'))
-    .map(({ total }) => Math.abs(total));
+  if (dataBASE.categoriesSummary.length !== 0) {
+    visible = true;
+    DiagramaItem = dataBASE.categoriesSummary.filter(({ type }) =>
+      type.includes('EXPENSE')
+    );
 
-  const TitleExpense = dataBASE.categoriesSummary
-    .filter(({ type }) => type.includes('EXPENSE'))
-    .map(({ name }) => name);
+    ExpenseSum = dataBASE.categoriesSummary
+      .filter(({ type }) => type.includes('EXPENSE'))
+      .map(({ total }) => Math.abs(total));
 
-  const data = {
-    labels: [...TitleExpense],
-    color: [
-      '#FED057',
-      '#FFD8D0',
-      '#FD9498',
-      '#C5BAFF',
-      '#6E78E8',
-      '#4A56E2',
-      '#81E1FF',
-      '#24CCA7',
-      '#00AD84',
-    ],
-    datasets: [
-      {
-        label: '# of Votes',
-        data: [...ExpenseSum],
-        backgroundColor: [
-          '#FED057',
-          '#FFD8D0',
-          '#FD9498',
-          '#C5BAFF',
-          '#6E78E8',
-          '#4A56E2',
-          '#81E1FF',
-          '#24CCA7',
-          '#00AD84',
-        ],
-        borderColor: [
-          '#FED057',
-          '#FFD8D0',
-          '#FD9498',
-          '#C5BAFF',
-          '#6E78E8',
-          '#4A56E2',
-          '#81E1FF',
-          '#24CCA7',
-          '#00AD84',
-        ],
-        borderWidth: 1,
-      },
-    ],
-  };
+    TitleExpense = dataBASE.categoriesSummary
+      .filter(({ type }) => type.includes('EXPENSE'))
+      .map(({ name }) => name);
+
+    data = {
+      labels: [...TitleExpense],
+      color: [
+        '#FED057',
+        '#FFD8D0',
+        '#FD9498',
+        '#C5BAFF',
+        '#6E78E8',
+        '#4A56E2',
+        '#81E1FF',
+        '#24CCA7',
+        '#00AD84',
+      ],
+      datasets: [
+        {
+          label: '# of Votes',
+          data: [...ExpenseSum],
+          backgroundColor: [
+            '#FED057',
+            '#FFD8D0',
+            '#FD9498',
+            '#C5BAFF',
+            '#6E78E8',
+            '#4A56E2',
+            '#81E1FF',
+            '#24CCA7',
+            '#00AD84',
+          ],
+          borderColor: [
+            '#FED057',
+            '#FFD8D0',
+            '#FD9498',
+            '#C5BAFF',
+            '#6E78E8',
+            '#4A56E2',
+            '#81E1FF',
+            '#24CCA7',
+            '#00AD84',
+          ],
+          borderWidth: 1,
+        },
+      ],
+    };
+  }
 
   useEffect(() => {
-    console.log('use');
-    fetchTransactionsSummaryOfPeriod({ month, year });
+    dispatch(fetchTransactionsSummary());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    dispatch(fetchTransactionsSummaryOfPeriod({ month, year }));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [month, year]);
 
   const onChanged = el => {
@@ -102,9 +114,7 @@ const DiagramTab = () => {
     <div className={styles.statisticChart}>
       <div className={styles.diagram}>
         <h1 className={styles.diagramTitle}>Statistics</h1>
-
-        <Doughnut data={data} />
-
+        {visible ? <Doughnut data={data} /> : <p>Nothing</p>}
         <p className={styles.incomeSum}>
           <span>&#8372;</span>
           {dataBASE.incomeSummary}
@@ -151,7 +161,7 @@ const DiagramTab = () => {
             <option value="2029">2029</option>
           </select>
         </div>
-        {/*  */}
+
         <div className={styles.chart}>
           <div className={styles.title}>
             <div className={styles.titleItem}>
@@ -160,28 +170,30 @@ const DiagramTab = () => {
             </div>
           </div>
 
-          <ul className={styles.list}>
-            {DiagramaItem.map(({ name, type, total }, index) => (
-              <li key={nanoid()} className={styles.listItem}>
-                <div className={styles.expenseItem}>
-                  <p
-                    style={{
-                      width: 24,
-                      height: 24,
-                      backgroundColor: [data.color[index]],
-                    }}
-                  ></p>
-
-                  <div className={styles.expenseItemText}>
-                    <span className={styles.nameExpense}>{name}</span>
-                    <span>{Math.abs(total)}</span>
+          {visible ? (
+            <ul className={styles.list}>
+              {DiagramaItem.map(({ name, type, total }, index) => (
+                <li key={nanoid()} className={styles.listItem}>
+                  <div className={styles.expenseItem}>
+                    <p
+                      style={{
+                        width: 24,
+                        height: 24,
+                        backgroundColor: [data.color[index]],
+                      }}
+                    ></p>
+                    <div className={styles.expenseItemText}>
+                      <span className={styles.nameExpense}>{name}</span>
+                      <span>{Math.abs(total)}</span>
+                    </div>
                   </div>
-                </div>
-
-                <div className={styles.separator}></div>
-              </li>
-            ))}
-          </ul>
+                  <div className={styles.separator}></div>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p className={styles.ListEmpty}>List expenses empty</p>
+          )}
 
           <div className={styles.totalList}>
             <p className={styles.totalListItem}>
