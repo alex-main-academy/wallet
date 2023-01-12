2810640889bfa6886ccfd5967f0b53453b73e93d
Lukast2008 <lukast2008@gmail.com>
2023-01-12 13:02:15 +0200

    diacrama

--- src/components/DiagramTab/DiagramTab.jsx
+++ src/components/DiagramTab/DiagramTab.jsx
@@ -1,5 +1,260 @@
+import { useEffect, useState } from 'react';
+import { Chart as ChartJS, ArcElement, Tooltip } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+import { nanoid } from '@reduxjs/toolkit';
+import styles from './DiagramTab.module.css';
+import { fetchTransactionsSummaryOfPeriod } from './operation';
+import { useSelector } from 'react-redux';
+// import { selectStatistic } from './Selector';
+// import { useSelector } from 'react-redux';
+import { selectStatistic } from './Selector';
+// import { Link, useLocation, useSearchParams } from 'react-router-dom';
+
+ChartJS.register(ArcElement, Tooltip);
+
 const DiagramTab = () => {
-  return <>DiagramTab</>;
+
+  // const [searchParams, setSearchParams] = useSearchParams();
+  // const username = useSearchParams.get('query');
+  // const location = useLocation();
+
+
+
+  const [month, setMonth] = useState('');
+  const [year, SetYear] = useState('2023');
+
+  const dataBASE = useSelector(selectStatistic)
+
+  // const dataBASE = {
+  //   categoriesSummary: [
+  //     {
+  //       name: 'Income',
+  //       type: 'INCOME',
+  //       total: 4000,
+  //     },
+  //     {
+  //       name: 'Education',
+  //       type: 'EXPENSE',
+  //       total: -274,
+  //     },
+  //     {
+  //       name: 'Household products',
+  //       type: 'EXPENSE',
+  //       total: -284,
+  //     },
+  //     {
+  //       name: 'Products',
+  //       type: 'EXPENSE',
+  //       total: -700,
+  //     },
+  //     {
+  //       name: 'Car',
+  //       type: 'EXPENSE',
+  //       total: -70,
+  //     },
+  //     {
+  //       name: 'Child care',
+  //       type: 'EXPENSE',
+  //       total: -285,
+  //     },
+  //     {
+  //       name: 'Self care',
+  //       type: 'EXPENSE',
+  //       total: -28,
+  //     },
+  //     {
+  //       name: 'Leisure',
+  //       type: 'EXPENSE',
+  //       total: -480,
+  //     },
+  //     {
+  //       name: 'Main expenses',
+  //       type: 'EXPENSE',
+  //       total: -2000,
+  //     },
+  //   ],
+  //   incomeSummary: 4000,
+  //   expenseSummary: -3245,
+  //   periodTotal: 755,
+  //   year: null,
+  //   month: null,
+  // };
+
+  const DiagramaItem = dataBASE.categoriesSummary.filter(({ type }) =>
+    type.includes('EXPENSE')
+  );
+
+  const ExpenseSum = dataBASE.categoriesSummary
+    .filter(({ type }) => type.includes('EXPENSE'))
+    .map(({ total }) => Math.abs(total));
+
+  const TitleExpense = dataBASE.categoriesSummary
+    .filter(({ type }) => type.includes('EXPENSE'))
+    .map(({ name }) => name);
+
+  const data = {
+    labels: [...TitleExpense],
+    color: [
+      '#FED057',
+      '#FFD8D0',
+      '#FD9498',
+      '#C5BAFF',
+      '#6E78E8',
+      '#4A56E2',
+      '#81E1FF',
+      '#24CCA7',
+      '#00AD84',
+    ],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [...ExpenseSum],
+        backgroundColor: [
+          '#FED057',
+          '#FFD8D0',
+          '#FD9498',
+          '#C5BAFF',
+          '#6E78E8',
+          '#4A56E2',
+          '#81E1FF',
+          '#24CCA7',
+          '#00AD84',
+        ],
+        borderColor: [
+          '#FED057',
+          '#FFD8D0',
+          '#FD9498',
+          '#C5BAFF',
+          '#6E78E8',
+          '#4A56E2',
+          '#81E1FF',
+          '#24CCA7',
+          '#00AD84',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  useEffect(() => {
+    console.log('use');
+    fetchTransactionsSummaryOfPeriod({ month, year });
+  }, [month, year]);
+
+  const onChanged = el => {
+    switch (el.target.id) {
+      case 'month':
+        setMonth(el.target.value);
+        break;
+      case 'year':
+        SetYear(el.target.value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <div className={styles.statisticChart}>
+      <div className={styles.diagram}>
+        <h1 className={styles.diagramTitle}>Statistics</h1>
+
+        <Doughnut data={data} />
+
+        <p className={styles.incomeSum}>
+          <span>&#8372;</span>
+          {dataBASE.incomeSummary}
+        </p>
+      </div>
+
+      <div className={styles.chartItem}>
+        <div className={styles.select}>
+          <select
+            className={styles.selectItem}
+            name="month"
+            id="month"
+            onChange={onChanged}
+          >
+            <option value="january">January</option>
+            <option value="february">February</option>
+            <option value="march">March</option>
+            <option value="april">April</option>
+            <option value="april">May</option>
+            <option value="april">June</option>
+            <option value="july">July</option>
+            <option value="august">August</option>
+            <option value="september">September</option>
+            <option value="november">October</option>
+            <option value="october">November</option>
+            <option value="december">December</option>
+          </select>
+          <select
+            className={styles.selectItem}
+            name="year"
+            id="year"
+            onChange={onChanged}
+          >
+            <option value="2019">2019</option>
+            <option value="2020">2020</option>
+            <option value="2021">2021</option>
+            <option value="2022">2022</option>
+            <option value="2023">2023</option>
+            <option value="2024">2024</option>
+            <option value="2025">2025</option>
+            <option value="2026">2026</option>
+            <option value="2027">2027</option>
+            <option value="2028">2028</option>
+            <option value="2029">2029</option>
+          </select>
+        </div>
+        {/*  */}
+        <div className={styles.chart}>
+          <div className={styles.title}>
+            <div className={styles.titleItem}>
+              <p>Category</p>
+              <p>Sum</p>
+            </div>
+          </div>
+
+          <ul className={styles.list}>
+            {DiagramaItem.map(({ name, type, total }, index) => (
+              <li key={nanoid()} className={styles.listItem}>
+                <div className={styles.expenseItem}>
+                  <p
+                    style={{
+                      width: 24,
+                      height: 24,
+                      backgroundColor: [data.color[index]],
+                    }}
+                  ></p>
+
+                  <div className={styles.expenseItemText}>
+                    <span className={styles.nameExpense}>{name}</span>
+                    <span>{Math.abs(total)}</span>
+                  </div>
+                </div>
+
+                <div className={styles.separator}></div>
+              </li>
+            ))}
+          </ul>
+
+          <div className={styles.totalList}>
+            <p className={styles.totalListItem}>
+              <span className={styles.result}>Expenses:</span>
+              <span className={styles.exp}>
+                {Math.abs(dataBASE.expenseSummary)}
+              </span>
+            </p>
+            <p className={styles.totalListItem}>
+              <span className={styles.result}>Income:</span>
+              <span className={styles.income}> {dataBASE.periodTotal}</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DiagramTab;
