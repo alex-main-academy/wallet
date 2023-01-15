6cbee7dca83ef4a46030a877080c8954903bb1a4
Alina Romanova <alinakhraban@gmail.com>
2023-01-15 14:59:05 +0200

    added validation

--- src/components/ModalAddTransaction/FormicDatetime.jsx
+++ src/components/ModalAddTransaction/FormicDatetime.jsx
@@ -1,27 +1,22 @@
 import Datetime from 'react-datetime';
 import moment from 'moment';
 
-const DATE_FORMAT = 'MM.DD.YYYY'
+const DATE_FORMAT = 'YYYY-MM-DD';
 
 const FormikDateTime = ({ field, form, timeFormat }) => {
   const onFieldChange = value => {
     let dateValue = value;
 
-    // if the date field isn't in a valid date format,
-    // react-datetime's onChange handler returns a string
-    // otherwise it returns a moment object
-    // this is why we can't override DateTime's onChange
-    // prop with Formik's field.onChange
     if (value instanceof moment) {
       dateValue = moment(value).format(DATE_FORMAT);
     }
 
     form.setFieldValue(field.name, dateValue);
-  }
+  };
 
   const onFieldBlur = () => {
     form.setFieldTouched(field.name, true);
-  }
+  };
 
   return (
     <Datetime
@@ -34,6 +29,6 @@ const FormikDateTime = ({ field, form, timeFormat }) => {
       onBlur={onFieldBlur}
     />
   );
-}
+};
 
 export default FormikDateTime;
--- src/components/ModalAddTransaction/ModalAddTransaction.jsx
+++ src/components/ModalAddTransaction/ModalAddTransaction.jsx
@@ -1,6 +1,6 @@
 import css from './ModalAddTransaction.module.css';
-import { Formik, Form, Field, ErrorMessage} from 'formik';
-import { transactionSchema} from './transaction_validation';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { transactionSchema } from './transaction_validation';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Select from 'react-select';
@@ -17,70 +17,48 @@ import { selectTransactionCategories } from 'redux/transactions/transactionsSele
 import { refreshUser } from 'redux/auth/authOperations';
 import FormikDateTime from './FormicDatetime';
 
-
 const ModalAddTransaction = ({ onClose, onClickBackdrop }) => {
-  // const [transactionDate, setTransactionDate] = useState(new Date());
-  // const [amount, setAmount] = useState('');
-  // const [comment, setComment] = useState('');
+
   const [type, setType] = useState('EXPENSE');
-  const [categoryId, setCategoryId] = useState('063f1132-ba5d-42b4-951d-44011ca46262');
+  const [categoryId, setCategoryId] = useState('');
   const [isToggled, setIsToggled] = useState(false);
 
   const dispatch = useDispatch();
   const categories = useSelector(selectTransactionCategories);
 
-
-  const onToggle = () => {
+  const onToggle = (setFieldValue, resetForm, values) => {
     setIsToggled(!isToggled);
-    if (isToggled) {
-      setType('EXPENSE');
+    console.log(values.target.checked);
+    if (values.target.checked) {
+      setType(type);
     } else {
       setType('INCOME');
       setCategoryId(categoryId);
     }
+
+    resetForm();
+    return setFieldValue('type', values.target.checked ? 'INCOME' : 'EXPENSE');
   };
 
   useEffect(() => {
     dispatch(fetchTransactionCategories());
   }, [dispatch]);
 
-  // const handleNameChange = e => {
-  //   const { name, value } = e.target;
-  //   switch (name) {
-  //     case 'amount':
-  //       setAmount(value);
-  //       break;
-
-  //     case 'comment':
-  //       setComment(value);
-  //       break;
-
-  //     default:
-  //       return;
-  //   }
-  // };
-
-  // const handleChangeDate = event => {
-  //   setTransactionDate(event);
-  // };
-
-  // const handleChangeCategories = event => {
-  //   setCategoryId(event.value);
-  // };
-
   const handlerSubmit = async ({
     transactionDate,
     type,
     categoryId,
     comment,
-    amount}) => {
+    amount,
+  }, resetForm) => {
+
 
     const correctAmmount = type === 'EXPENSE' ? Number('-' + amount) : amount;
 
     try {
       await dispatch(
         addTransaction({
-          transactionDate: new Date(),
+          transactionDate: new Date(transactionDate),
           type,
           categoryId,
           comment,
@@ -93,15 +71,17 @@ const ModalAddTransaction = ({ onClose, onClickBackdrop }) => {
     }
 
     dispatch(refreshUser());
-    // setTransactionDate(new Date());
-    // setType('EXPENSE');
-    // setCategoryId('');
-    // setComment('');
-    // setAmount('');
+    resetForm();
+
   };
 
 
-return (
+  const handleCancel = (e) => {
+
+    console.log(handleCancel)
+};
+
+  return (
     <div className={css.overlay} onClick={onClickBackdrop}>
       <div className={css.modal}>
         <button type="button" className={css.modalCloseBtn} onClick={onClose}>
@@ -113,125 +93,135 @@ return (
         </button>
         <h2 className={css.modalTitle}>Add transaction</h2>
         <Formik
-        initialValues={{
-        type: 'EXPENSE',
-        amount: '',
-        categoryId: '063f1132-ba5d-42b4-951d-44011ca46262',
-        transactionDate: new Date(),
-        comment: '',}}
-        validationSchema={transactionSchema}
-        onSubmit={(values, { setSubmitting,resetForm }) =>
-        {
+          initialValues={{
+            type: 'EXPENSE',
+            amount: '',
+            categoryId: '063f1132-ba5d-42b4-951d-44011ca46262',
+            transactionDate: new Date(),
+            comment: '',
+          }}
+          validationSchema={transactionSchema}
+          onSubmit={(values) => {
             handlerSubmit(values);
-            setSubmitting(false);
-            resetForm()
-        }}
+          }}
         >
-         {formik => (
-        <Form >
-          <div className={css.modalWrappenTransaction}>
-            {isToggled ? (
-              <p className={css.activeTransactionIncome}>Income</p>
-            ) : (
-              <p className={css.modalTransactionIncome}>Income</p>
-            )}
-            <label className={css.toggleSwitch}>
-              <input
-                type="checkbox"
-                name='type'
-                value={type}
-                checked={isToggled}
-                onChange={onToggle}
+          {formik => (
+            <Form>
+              <div className={css.modalWrappenTransaction}>
+                {isToggled ? (
+                  <p className={css.activeTransactionIncome}>Income</p>
+                ) : (
+                  <p className={css.modalTransactionIncome}>Income</p>
+                )}
+                <label className={css.toggleSwitch}>
+                  <Field
+                    type="checkbox"
+                    name="type"
+                    checked={isToggled}
+                    onChange={values =>
+                      onToggle(formik.setFieldValue, formik.resetForm, values)
+                    }
+                  />
+                  <span className={css.switch} />
+                </label>
+                {isToggled ? (
+                  <p className={css.modalTransactionExpense}>Expense</p>
+                ) : (
+                  <p className={css.activeTransactionExpense}>Expense</p>
+                )}
+              </div>
+              {!isToggled && (
+                <Field name="categoryId">
+                  {({ field, form }) => (
+                    <Select
+                      onChange={selectedOption =>
+                        form.setFieldValue('categoryId', selectedOption.value)
+                      }
+                      className={css.modalSelect}
+                      placeholder={
+                        <div className={css.selectPlaceholderText}>
+                          Select a category
+                        </div>
+                      }
+                      options={categories
+                        .filter(category => category.type === form.values.type)
+                        .map(category => ({
+                          value: category.id,
+                          label: category.name,
+                        }))}
+                      theme={theme => ({
+                        ...theme,
+                        borderRadius: '20px',
+                        background: 'rgba(255, 255, 255, 0.7)',
+                        boxShadow: '0px 6px 15px rgba(0, 0, 0, 0.1)',
+                        colors: {
+                          ...theme.colors,
+                          text: '#FF6596',
+                          primary25: 'white',
+                          primary: '#FF6596',
+                        },
+                      })}
+                      styles={{
+                        control: (baseStyles, state) => ({
+                          ...baseStyles,
+                          border: 'none',
+                          borderBottom: ' 1px solid #e0e0e0',
+                          outline: 'none',
+                        }),
+                      }}
+                    />
+                  )}
+                </Field>
+              )}
+              <ErrorMessage
+                name="categoryId"
+                component="div"
+                className={css.invalidFeedback}
               />
-              <span className={css.switch} />
-            </label>
-            {isToggled ? (
-              <p className={css.modalTransactionExpense}>Expense</p>
-            ) : (
-              <p className={css.activeTransactionExpense}>Expense</p>
-            )}
-          </div>
-          {!isToggled && (
-            <Field name="categoryId">
-             {({field,form})=>(
-            <Select
-            onChange={(selectedOption)=> form.setFieldValue('categoryId',selectedOption.value)}
-              className={css.modalSelect}
-              placeholder={
-                <div className={css.selectPlaceholderText}>
-                  Select a category
-                </div>
-              }
-              options={categories
-                .filter(category => category.type === type)
-                .map(category => ({
-                  value: category.id,
-                  label: category.name,
-                }))}
-              theme={theme => ({
-                ...theme,
-                borderRadius: '20px',
-                background: 'rgba(255, 255, 255, 0.7)',
-                boxShadow: '0px 6px 15px rgba(0, 0, 0, 0.1)',
-                colors: {
-                  ...theme.colors,
-                  text: '#FF6596',
-                  primary25: 'white',
-                  primary: '#FF6596',
-                },
-              })}
-              styles={{
-                control: (baseStyles, state) => ({
-                  ...baseStyles,
-                  border: 'none',
-                  borderBottom: ' 1px solid #e0e0e0',
-                  outline: 'none',
-                }),
-              }}
-            />)}
-            </Field>
-          )}
-           <ErrorMessage
-                  name="categoryId"
-                  component="div"
-                  className={css.invalidFeedback}
+              <div className={css.modalWrapper}>
+                <Field
+                  className={css.formInputSum}
+                  type="number"
+                  name="amount"
+                  placeholder="0.00"
                 />
-          <div className={css.modalWrapper}>
-            <Field
-              className={css.formInputSum}
-              type="number"
-              name="amount"
-              placeholder="0.00"
-            />
-            <ErrorMessage
+                <ErrorMessage
                   name="amount"
                   component="div"
                   className={css.invalidFeedback}
                 />
-            <div className={css.inputDatetime}>
-              <Field name="transactionDate"  timeFormat={false} component={FormikDateTime}/>
-              <ErrorMessage
-                  name="transactionDate"
-                  component="div"
-                  className={css.invalidFeedback}
-                />
-              <img className={css.calendarIcon} src={calendar} alt="calendar" />
-            </div>
-          </div>
-          <Field
-            className={css.inputCommentText}
-            type="text"
-            name="comment"
-            placeholder="Comment"
-          />
-          <button className={css.btnAdd} type="submit">Add</button>
-        </Form>
-         )}
+                <div className={css.inputDatetime}>
+                  <Field
+                    name="transactionDate"
+                    timeFormat={false}
+                    component={FormikDateTime}
+                  />
+                  <ErrorMessage
+                    name="transactionDate"
+                    component="div"
+                    className={css.invalidFeedback}
+                  />
+                  <img
+                    className={css.calendarIcon}
+                    src={calendar}
+                    alt="calendar"
+                  />
+                </div>
+              </div>
+              <Field
+                className={css.inputCommentText}
+                type="text"
+                name="comment"
+                placeholder="Comment"
+              />
+              <button className={css.btnAdd} type="submit">
+                Add
+              </button>
+            </Form>
+          )}
         </Formik>
 
-        <button className={css.btnCancel} >
-        Cancel
-          </button>
+        <button className={css.btnCancel} onClick={handleCancel}>Cancel</button>
       </div>
       <ToastContainer />
     </div>
